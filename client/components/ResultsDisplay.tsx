af5c86db049c37207975c92ccd308d0cab1b352e
Nicolai Andre Dalaaker <nicolaad@stud.ntnu.no>
2020-11-15 16:42:47 +0100

    added working search on querys of kommune and fylke

--- client/components/ResultsDisplay.tsx
+++ client/components/ResultsDisplay.tsx
@@ -3,6 +3,8 @@ import { ScrollView } from "react-native-gesture-handler";
 import BomCard from "./BomCard";
 import { useQuery, gql } from "@apollo/client";
 import { ActivityIndicator, Text } from "react-native";
+import { useSelector } from "react-redux";
+import { selectText, selectType } from "./state/filterSlice";
 
 type bomData = {
   id: string;
@@ -12,47 +14,77 @@ type bomData = {
   TAKST_LITEN_BIL: Number;
   TAKST_STOR_BIL: Number;
 };
-const BOMSTASJONER = gql`
-  {
-    bomstasjoner {
-      id
-      NAVN_BOMSTASJON
-      FYLKE
-      TAKST_STOR_BIL
-      TAKST_LITEN_BIL
-    }
-  }
-`;
 
 //TODO: make it fetch with apollo here, baswed on redux state!
 const ResultDisplay: React.FC = () => {
-  const { loading, data, fetchMore } = useQuery(BOMSTASJONER, {
-    variables: {
-      offset: 0,
-      limit: 10,
-    },
-  });
-  if (!loading) {
-    console.log(data.bomstasjoner);
+  const filterType = useSelector(selectType);
+  const filterText = useSelector(selectText);
+  let query;
+
+  //Prepares query based on filtertype. TODO: extract this to tidy up file
+  if (filterType == "Fylke") {
+    query = gql`
+      query boms($text: String) {
+        bomstasjoner: bomstasjonerByFylke(FYLKE: $text) {
+          id
+          NAVN_BOMSTASJON
+          FYLKE
+          KOMMUNE
+          TAKST_STOR_BIL
+          TAKST_LITEN_BIL
+        }
+      }
+    `;
+  } else if (filterType == "Kommune") {
+    query = gql`
+      query boms($text: String) {
+        bomstasjoner: bomstasjonerByKommune(KOMMUNE: $text) {
+          id
+          NAVN_BOMSTASJON
+          FYLKE
+          KOMMUNE
+          TAKST_STOR_BIL
+          TAKST_LITEN_BIL
+        }
+      }
+    `;
+  } else {
+    query = null;
   }
 
-  if (loading) {
-    return <ActivityIndicator size="large" />;
+  //if for some reason a invalid query is sent
+  if (query == null) {
+    return (
+      <ScrollView>
+        <Text>Your search is not valid</Text>
+      </ScrollView>
+    );
+  } else {
+    const { loading, data, fetchMore } = useQuery(query, {
+      variables: {
+        text: filterText,
+      },
+    });
+
+    if (loading) {
+      return <ActivityIndicator size="large" />;
+    }
+
+    return (
+      <ScrollView>
+        {data.bomstasjoner.map((bomData: bomData) => (
+          <BomCard
+            key={bomData.id}
+            name={bomData.NAVN_BOMSTASJON}
+            fylke={bomData.FYLKE}
+            kommune={bomData.KOMMUNE}
+            carPrice={bomData.TAKST_LITEN_BIL}
+            truckPrice={bomData.TAKST_STOR_BIL}
+          ></BomCard>
+        ))}
+      </ScrollView>
+    );
   }
-  return (
-    <ScrollView>
-      {data.bomstasjoner.map((bomData: bomData) => (
-        <BomCard
-          key={bomData.id}
-          name={bomData.NAVN_BOMSTASJON}
-          fylke={bomData.FYLKE}
-          kommune={bomData.KOMMUNE}
-          carPrice={bomData.TAKST_LITEN_BIL}
-          truckPrice={bomData.TAKST_STOR_BIL}
-        ></BomCard>
-      ))}
-    </ScrollView>
-  );
 };
 
 export default ResultDisplay;
